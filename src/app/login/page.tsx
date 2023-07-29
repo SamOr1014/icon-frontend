031cf67457e4d1f4a144e2baf3acf03312eb17b5
SamOr1014 <orchamsumsam1014@gmail.com>
2023-07-29 17:34:35 +0800

    feat: register page with completed form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,13 @@
-import { LoginCard } from "./_components/LoginCard";
+import LoginForm from "./_components/LoginForm";
+import LoginTitle from "./_components/Title";
 
-export default function Login() {
-  return <LoginCard />;
-}
+export const Login = () => {
+  return (
+    <div className="flex flex-col gap-10 min-w-[350px] md:min-w-[500px] min-h-[500px] p-5 rounded-lg shadow-lg shadow-white">
+      <LoginTitle />
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/FormInput.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React, {
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  PropsWithRef,
+  forwardRef,
+} from "react";
+
+type Props = {
+  //type the colour for tailwind styling
+  inputBgColour?: `#${string}` | `${string}-${string}`;
+} & DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> &
+  PropsWithRef<any>;
+
+const FormInput = forwardRef(({ inputBgColour, ...restProps }: Props, ref) => {
+  return (
+    <div>
+      <input
+        ref={ref}
+        {...restProps}
+        className={`w-full input input-bordered ${
+          inputBgColour ? `bg-[${inputBgColour}]` : "bg-gray-700"
+        }`}
+      />
+    </div>
+  );
+});
+
+FormInput.displayName = "FormInput";
+export default FormInput;
